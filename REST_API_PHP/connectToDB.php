748e3f4da9f36442884fcb0409ecc7735d1c2c59
Daniel Makgonta <Daniel Makgonta@196.248.100.3>
2015-08-18 19:06:44 +0200

    Adding REST API for communication between applications

---
+++ REST_API_PHP/connectToDB.php
@@ -0,0 +1,14 @@
+<?php
+
+    $DB_SERVER = "localhost";
+    $DB_USER = "root";
+    $DB_PASSWORD = "";
+    $DB = "LookyFindMedb";
+    
+    $db = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB);
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
+    
