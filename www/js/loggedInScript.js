81f822805cc4363df4c16e5b27401e2f05b82c66
Daniel Makgonta <Daniel Makgonta@196.248.115.210>
2015-08-24 20:35:31 +0200

    Added functionality to front end

--- www/js/loggedInScript.js
+++ www/js/loggedInScript.js
@@ -10,75 +10,7 @@ $(document).on("pageshow", "#loggedIn", function () {
     $('#recentPendingListView').children().remove();
     $('#recentRequestsListView').children().remove();
 
-    jQuery.ajax({
-        type: "POST",
-        url: "http://localhost:8001/",
-        data: {method: "getfriends", username: username},
-        success: function (data, status, jqXHR) {
-            if (data.success === true) {
-                if (data.data === null) {
-                    $('#options').append('<p style="color:red;"> No friends. </p>');
-                } else {
-                    var obj = JSON.parse(data.data);
-                    obj.forEach(function (item) {
-                        $('#recentContactListView').append("<li><a href='#' class='ui-btn' id=" + item + ">" + item + "</a></li>");
-                    });
-                }
-            } else {
-                $('#options').append('<p> No friends. </p>');
-            }
-        },
-        error: function (jqXHR, status) {
-            alert('An unexpected error has occurred. ' + status);
-        }
-    });
-
-    jQuery.ajax({
-        type: "POST",
-        url: "http://localhost:8001/",
-        data: {method: "getPending", username: username},
-        success: function (data, status, jqXHR) {
-            if (data.success === true) {
-                if (data.data === null) {
-                    $('#options').append('<p style="color:red;"> No pending friends. </p>');
-                } else {
-                    var obj = JSON.parse(data.data);
-                    obj.forEach(function (item) {
-                        $('#recentPendingListView').append("<li><a href='#' class='ui-btn' id=" + item + ">" + item + "</a></li>");
-                    });
-                }
-            } else {
-                $('#options').append('<p> No pending friends. </p>');
-            }
-        },
-        error: function (jqXHR, status) {
-            alert('An unexpected error has occurred. ' + status);
-        }
-    });
-
-
-    jQuery.ajax({
-        type: "POST",
-        url: "http://localhost:8001/",
-        data: {method: "getRequests", username: username},
-        success: function (data, status, jqXHR) {
-            if (data.success === true) {
-                if (data.data === null) {
-                    $('#options').append('<p style="color:red;"> No requests. </p>');
-                } else {
-                    var obj = JSON.parse(data.data);
-                    obj.forEach(function (item) {
-                        $('#recentRequestsListView').append("<li><a href='#' class='ui-btn' id=" + item + ">" + item + "</a></li>");
-                    });
-                }
-            } else {
-                $('#options').append('<p> No requests. </p>');
-            }
-        },
-        error: function (jqXHR, status) {
-            alert('An unexpected error has occurred. ' + status);
-        }
-    });
+    loadLists(username);
 
     $("#recentContactListView").listview('refresh');
     $("#recentPendingListView").listview('refresh');
@@ -165,8 +97,17 @@ $(document).on("pagecreate", "#loggedIn", function () {
                 if (data.success === true) {
                     $('#acceptedFriendConfirmBox').html("<p style='color:green;text-align:center'>Friend Added.</p>");
                     setTimeout(function () {
+                        $('#recentContactListView').children().remove();
+                        $('#recentPendingListView').children().remove();
+                        $('#recentRequestsListView').children().remove();
+
+                        loadLists(username);
+
+                        $("#recentContactListView").listview('refresh');
+                        $("#recentPendingListView").listview('refresh');
+                        $("#recentRequestsListView").listview('refresh');
                         $('#acceptedFriendConfirmBox').html("").fadeOut();
-                    }, 1500);
+                    }, 3000);
                 } else {
                     $('#acceptedFriendConfirmBox').html("<p style='color:red;text-align:center'>Friend could not be added. </p>");
                     setTimeout(function () {
@@ -187,6 +128,161 @@ $(document).on("pagecreate", "#loggedIn", function () {
     $('#cancelFriend').click(function (event) {
         event.preventDefault();
         $.setCookie('friend', null, -1);
-        $.mobile.navigate(this.attr("href"));
+        $.mobile.navigate($(this).attr("href"));
+    });
+
+    $('#recentContactListView').delegate('li', 'tap', function () {
+        var username = $(this).find('a').attr('id');
+        $.setCookie("friend", username, 1);
+        $('#findFriendHeader').html("Find " + username + "?");
+        $('#confirmFindFriend').popup("open");
+    });
+
+
+    $('#findFriend').click(function (event) {
+        event.preventDefault();
+
+        var friend = $.getCookie('friend').trim();
+        var coordinates = null;
+
+        jQuery.ajax({
+            type: "POST",
+            url: "http://localhost:8001/",
+            data: {method: "findFriend", coordinates: coordinates, friend: friend},
+            success: function (data, status, jqXHR) {
+
+                if (data.success === true) {
+
+                } else {
+
+                }
+
+            },
+            error: function (jqXHR, status) {
+                alert('An unexpected error has occurred.');
+            }
+        });
+
+        $.setCookie('friend', null, -1);
+        $.mobile.changePage('#loggedIn');
+    });
+
+    $('#cancelFindFriend').click(function (event) {
+        event.preventDefault();
+        $.setCookie('friend', null, -1);
+        $.mobile.navigate($(this).attr("href"));
+    });
+
+    $('#unFriend').click(function (event) {
+        event.preventDefault();
+
+        var friend = $.getCookie('friend');
+        var username = $.getCookie('username');
+
+        jQuery.ajax({
+            type: "POST",
+            url: "http://localhost:8001/",
+            data: {method: "unFriend", username: username, friend: friend},
+            success: function (data, status, jqXHR) {
+                if (data.success === true) {
+                    $('#acceptedFriendConfirmBox').html("<p style='color:green;text-align:center'>Unfriended.</p>");
+                    setTimeout(function () {
+                        $('#recentContactListView').children().remove();
+                        $('#recentPendingListView').children().remove();
+                        $('#recentRequestsListView').children().remove();
+
+                        loadLists(username);
+
+                        $("#recentContactListView").listview('refresh');
+                        $("#recentPendingListView").listview('refresh');
+                        $("#recentRequestsListView").listview('refresh');
+                        $('#acceptedFriendConfirmBox').html("").fadeOut();
+                    }, 3000);
+                } else {
+                    $('#acceptedFriendConfirmBox').html("<p style='color:redF;text-align:center'>Could not be unfriended.</p>");
+                    setTimeout(function () {
+                    }, 3000);
+                }
+            },
+            error: function (jqXHR, status) {
+                alert('An unexpected error has occurred.');
+            }
+        });
+        $.setCookie('friend', null, -1);
+        $.mobile.navigate($(this).attr("href"));
     });
 });
+
+
+function loadLists(username) {
+    jQuery.ajax({
+        type: "POST",
+        url: "http://localhost:8001/",
+        data: {method: "getfriends", username: username},
+        success: function (data, status, jqXHR) {
+            if (data.success === true) {
+                if (data.data === null) {
+                    $('#options').append('<p style="color:red;"> No friends. </p>');
+                } else {
+                    var obj = JSON.parse(data.data);
+                    obj.forEach(function (item) {
+                        $('#recentContactListView').append("<li><a href='#' class='ui-btn' id=" + item + ">" + item + "</a></li>");
+                    });
+                }
+            } else {
+                $('#options').append('<p> No friends. </p>');
+            }
+        },
+        error: function (jqXHR, status) {
+            alert('An unexpected error has occurred. ' + status);
+        }
+    });
+
+    jQuery.ajax({
+        type: "POST",
+        url: "http://localhost:8001/",
+        data: {method: "getPending", username: username},
+        success: function (data, status, jqXHR) {
+            if (data.success === true) {
+                if (data.data === null) {
+                    $('#options').append('<p style="color:red;"> No pending friends. </p>');
+                } else {
+                    var obj = JSON.parse(data.data);
+                    obj.forEach(function (item) {
+                        $('#recentPendingListView').append("<li><a href='#' class='ui-btn' id=" + item + ">" + item + "</a></li>");
+                    });
+                }
+            } else {
+                $('#options').append('<p> No pending friends. </p>');
+            }
+        },
+        error: function (jqXHR, status) {
+            alert('An unexpected error has occurred. ' + status);
+        }
+    });
+
+
+    jQuery.ajax({
+        type: "POST",
+        url: "http://localhost:8001/",
+        data: {method: "getRequests", username: username},
+        success: function (data, status, jqXHR) {
+            if (data.success === true) {
+                if (data.data === null) {
+                    $('#options').append('<p style="color:red;"> No requests. </p>');
+                } else {
+                    var obj = JSON.parse(data.data);
+                    obj.forEach(function (item) {
+                        $('#recentRequestsListView').append("<li><a href='#' class='ui-btn' id=" + item + ">" + item + "</a></li>");
+                    });
+                }
+            } else {
+                $('#options').append('<p> No requests. </p>');
+            }
+        },
+        error: function (jqXHR, status) {
+            alert('An unexpected error has occurred. ' + status);
+        }
+    });
+}
+;
